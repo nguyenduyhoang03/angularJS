c70da5fe28749c043084a712da18170cfa8973ef
nguyenduyhoang03 <129242179+nguyenduyhoang03@users.noreply.github.com>
2023-05-20 12:50:24 +0700

    Update app.js

--- examAngularJS/app.js
+++ examAngularJS/app.js
@@ -12,16 +12,3 @@ app.controller('registerCtrl', function($scope) {
   };
   
 });
-
-app.controller('loginCtrl',function($scope){
-    $scope.login = function() {
-        // Kiểm tra xem email và password có hợp lệ hay không
-        if ($scope.user.email === 'example@gmail.com' && $scope.user.password === '123456') {
-            // Nếu hợp lệ, chuyển đến trang chính
-            window.location.href = 'main.html';
-        } else {
-            // Nếu không hợp lệ, hiển thị thông báo lỗi
-            alert('Invalid email or password!');
-        }
-    };
-})
